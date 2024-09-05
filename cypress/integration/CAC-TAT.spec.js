6cf6c92db3ec6058879a0e66f4ec30f7e5366969
José Fernando dos Santos <josefernando@mpse.mp.br>
2024-09-05 10:46:04 -0300

    testes

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,18 @@
+// CAC-TAT.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+
+    beforeEach(() => cy.visit('./src/index.html'))
+
+    it('verifica o título da aplicação', function() {
+
+        cy.title().should('eq', 'Central de Atendimento ao Cliente TAT');
+          
+    })
+  })
